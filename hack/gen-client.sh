939e6165fe74e4cf6e5e5422ba18dd8a25e046a4
Google Cloud Platform <noreply@google.com>
2025-12-29 10:59:49 +0000

    Internal change.
    GitOrigin-RevId: 29467ca45a90de1ec1fb7f576ca93b1d947ff66a

--- hack/gen-client.sh
+++ hack/gen-client.sh
@@ -23,15 +23,14 @@ fi
 
 command -v controller-gen >/dev/null 2>&1 || { echo >&2 "controller-gen should be available in PATH, consider installing using 'make gen-clients-init'"; exit 1; }
 
-# Fetch code-generator repository
-TMP_DIR=$(mktemp -d)
-CODEGEN_PKG="${TMP_DIR}/code-generator"
-CODEGEN_REPO="https://github.com/kubernetes/code-generator.git"
-CODEGEN_VERSION="kubernetes-1.30.3"
+# Ensure that all dependencies are downloaded
+go mod download
 
-git clone --branch "${CODEGEN_VERSION}" --single-branch "${CODEGEN_REPO}" "${CODEGEN_PKG}"
+CODEGEN_PKG="k8s.io/code-generator"
+# In case this fails - you're missing code-gen module, run go mod tidy to download it
+CODEGEN_PATH=$(go list -m -f '{{.Dir}}' "${CODEGEN_PKG}")
 
-. "${CODEGEN_PKG}/kube_codegen.sh"
+. "${CODEGEN_PATH}/kube_codegen.sh"
 
 # Generate client
 BOILERPLATE_FILE="${ROOT}/hack/boilerplate.go.txt"
@@ -56,7 +55,7 @@ kube::codegen::gen_client "${API_PATH}" \
     --boilerplate "${BOILERPLATE_FILE}"
 
 echo "INFO: Generating CRD manifest..."
-controller-gen \
+go tool controller-gen \
   crd:generateEmbeddedObjectMeta=true \
   paths="${API_PATH}/..." \
   output:crd:dir="${ROOT}"
