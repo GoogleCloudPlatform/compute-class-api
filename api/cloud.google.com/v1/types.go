f8452197965ff78582996de0373af74d1e219cce
Google Cloud Platform <noreply@google.com>
2026-03-20 10:32:18 +0000

    Internal change.
    GitOrigin-RevId: c9ffe2da924e11a1f2eea0230dd81b42965563a2

--- api/cloud.google.com/v1/types.go
+++ api/cloud.google.com/v1/types.go
@@ -600,7 +600,7 @@ type Reservations struct {
 // Priority is a specification of preferred machine characteristics.
 //
 // +kubebuilder:validation:MinProperties=1
-// +kubebuilder:validation:XValidation:rule="!has(self.nodepools) || (size(dyn(self)) == 1 + (has(self.identifier) ? 1 : 0))", message="Nodepool field cannot be set along with other fields"
+// +kubebuilder:validation:XValidation:rule="has(self.nodepools) ? (size(dyn(self)) == 1) : true", message="Nodepool field cannot be set along with other fields"
 // +kubebuilder:validation:XValidation:rule="!(has(self.machineFamily) && has(self.machineType))",message="MachineFamily and MachineType cannot be set together"
 // +kubebuilder:validation:XValidation:rule="!(has(self.machineType) && (has(self.minCores) || has(self.minMemoryGb)))",message="MachineType cannot be set together with MinCores/MinMemoryGb"
 // +kubebuilder:validation:XValidation:rule="!(has(self.machineFamily) && self.machineFamily == 'ek')", message="MachineFamily cannot be equal to 'ek'"
